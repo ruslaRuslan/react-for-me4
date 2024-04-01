be45dcfb4f86bc9608af97812e1e25cae90dc476
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-01 19:13:58 +0400

    beyaxkinnen ferqi countries.map((country, index) ferqi

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -69,6 +69,46 @@ import "./index.css"
 
 // ----------------------------------------------------------------------------------------------------------------------------------
 
+// const url = "https://restcountries.com/v3.1/all" //close
+
+// const Countries = () => {
+//     const [countries, setCountries] = useState([])
+
+//     function deleteCountry(name) {
+//         setCountries((pre) => pre.filter((country) => country.name.common !== name))
+//     }
+
+//     useEffect(() => {
+//         axios.get(url).then(({ data }) => {
+//             setCountries(data)
+//         })
+//     }, [])
+
+
+//     useEffect(() => {
+//         console.log('countries state-i deyisdi');
+//     }, [countries])
+
+
+//     return (
+//         <div className="countries">
+//             {
+//                 countries.map((country) => {
+//                     return (
+//                         <div key={country.name.common}>
+//                             <img src={country.flags.png} alt="" />
+//                             <h3>{country.name.official} <button onClick={() => { deleteCountry(country.name.common) }} key={country.name.common}>x</button></h3>
+//                             <h4>{country.capital && country.capital[0]}</h4>
+//                         </div>
+//                     )
+//                 })
+//             }
+//         </div>
+//     )
+// }
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
 const url = "https://restcountries.com/v3.1/all" //close
 
 const Countries = () => {
@@ -93,9 +133,9 @@ const Countries = () => {
     return (
         <div className="countries">
             {
-                countries.map((country) => {
+                countries.map((country, index) => {
                     return (
-                        <div key={country.name.common}>
+                        <div key={index}>
                             <img src={country.flags.png} alt="" />
                             <h3>{country.name.official} <button onClick={() => { deleteCountry(country.name.common) }} key={country.name.common}>x</button></h3>
                             <h4>{country.capital && country.capital[0]}</h4>
@@ -106,5 +146,4 @@ const Countries = () => {
         </div>
     )
 }
-
 export default Countries;

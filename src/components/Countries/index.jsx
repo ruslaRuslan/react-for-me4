f9f3a2db2bb8fddabc82c0d31157aebcf99422b3
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-01 18:33:28 +0400

    x-i kilk edende silir

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -36,10 +36,44 @@ import "./index.css"
 
 // ------------------------------------------------------------------------------------------------------------------------------
 
+// const url = "https://restcountries.com/v3.1/all" //close
+
+// const Countries = () => {
+//     const [countries, setCountries] = useState([])
+//     function deleteCountry(name) {
+//         setCountries((pre) => pre.filter((contry) => contry.name.common !== name))
+//     }
+
+//     useEffect(() => {
+//         axios.get(url).then(({ data }) => {
+//             setCountries(data)
+//         })
+//     }, [])
+
+//     return (
+//         <div className="countries">
+//             {
+//                 countries.map((contry) => {
+//                     return (
+//                         <div onClick={() => { deleteCountry(contry.name.common) }} key={contry.name.common}>
+//                             <img src={contry.flags.png} alt="" />
+//                             <h3>{contry.name.official}</h3>
+//                             <h4>{contry.capital && contry.capital[0]}</h4>
+//                         </div>
+//                     )
+//                 })
+//             }
+//         </div>
+//     )
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
 const url = "https://restcountries.com/v3.1/all" //close
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
+
     function deleteCountry(name) {
         setCountries((pre) => pre.filter((contry) => contry.name.common !== name))
     }
@@ -55,9 +89,9 @@ const Countries = () => {
             {
                 countries.map((contry) => {
                     return (
-                        <div onClick={() => { deleteCountry(contry.name.common) }} key={contry.name.common}>
+                        <div>
                             <img src={contry.flags.png} alt="" />
-                            <h3>{contry.name.official}</h3>
+                            <h3>{contry.name.official} <button onClick={() => { deleteCountry(contry.name.common) }} key={contry.name.common}>x</button></h3>
                             <h4>{contry.capital && contry.capital[0]}</h4>
                         </div>
                     )

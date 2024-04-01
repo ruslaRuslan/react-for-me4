630704f3df88a3771f8041e44ec16c3522d39bf5
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-01 18:27:36 +0400

    beyaxkinin basqa bir formasi

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -2,13 +2,46 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css"
 
+// const url = "https://restcountries.com/v3.1/all" //close
+
+// const Countries = () => {
+//     const [countries, setCountries] = useState([])
+//     function deleteCountry(name) {
+//         const filteredCountries = countries.filter((contry) => contry.name.common !== name)
+//         setCountries(filteredCountries)
+//     }
+
+//     useEffect(() => {
+//         axios.get(url).then(({ data }) => {
+//             setCountries(data)
+//         })
+//     }, [])
+
+//     return (
+//         <div className="countries">
+//             {
+//                 countries.map((contry) => {
+//                     return (
+//                         <div onClick={() => { deleteCountry(contry.name.common) }} key={contry.name.common}>
+//                             <img src={contry.flags.png} alt="" />
+//                             <h3>{contry.name.official}</h3>
+//                             <h4>{contry.capital && contry.capital[0]}</h4>
+//                         </div>
+//                     )
+//                 })
+//             }
+//         </div>
+//     )
+// }
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
 const url = "https://restcountries.com/v3.1/all" //close
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
-    function deleteCountry(ad) {
-        const filteredCountries = countries.filter((contry)=> contry.name.common !== ad)
-       setCountries(filteredCountries)
+    function deleteCountry(name) {
+        setCountries((pre) => pre.filter((contry) => contry.name.common !== name))
     }
 
     useEffect(() => {
